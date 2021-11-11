6b81688a7f9ab9abc73b60eb62a9a63cb4812b55
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-11-11 14:12:44 +0900

    Update UXPin and Storybook Configurations

--- uxpin.webpack.config.js
+++ uxpin.webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 2
+              importLoaders: 2,
+              modules: true
             },
           },
         ],
